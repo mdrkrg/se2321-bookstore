7ebbcd9be1afccc41e2bd4fd555874d5ac1debe8
MidoriKurage <tonygong233@gmail.com>
2025-05-04 16:17:38 +0800

    deps(admin): added snap-admin as deps

--- src/main/java/me/crvena/bookstore/BookstoreApplication.java
+++ src/main/java/me/crvena/bookstore/BookstoreApplication.java
@@ -1,13 +1,15 @@
 package me.crvena.bookstore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
+import tech.ailef.snapadmin.external.*;
 
 @SpringBootApplication
+@ImportAutoConfiguration(SnapAdminAutoConfiguration.class)
 public class BookstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BookstoreApplication.class, args);
+  }
 
 }
