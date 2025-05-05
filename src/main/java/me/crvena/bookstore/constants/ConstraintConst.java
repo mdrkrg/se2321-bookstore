7036c1066f81ae6468fd1246755479cacadcac5f
MidoriKurage <tonygong233@gmail.com>
2025-05-05 09:25:15 +0800

    chore(models): moved constant to separate class

---
+++ src/main/java/me/crvena/bookstore/constants/ConstraintConst.java
@@ -0,0 +1,9 @@
+package me.crvena.bookstore.constants;
+
+public class ConstraintConst {
+
+  public static final int MAX_ADDRESS_LENGTH = 100;
+  public static final int MAX_NAME_LENGTH = 50;
+  public static final int MAX_TEL_LENGTH = 30;
+  public static final String PHONE_REGEX = "^[+]?[(]?[0-9]{1,4}[)]?[-\\s.0-9]{9,}$";
+}
